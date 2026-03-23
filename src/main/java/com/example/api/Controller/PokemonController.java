258f241dc0fe1632b8bda0fbc300692496aace2e
DYerpesruizgijon <david.yerpes-gordillo@iesruizgijon.com>
2026-03-23 10:42:44 +0100

    cambios necesario porfi no te rompas api

--- src/main/java/com/example/api/Controller/PokemonController.java
+++ src/main/java/com/example/api/Controller/PokemonController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,7 +21,7 @@ import com.example.api.Repository.PokemonRepository;
 
 @RestController
 @RequestMapping("/api/pokemons")
-@CrossOrigin(origins = "http://localhost:4200")
+@CrossOrigin(origins = "*", allowedHeaders = "*", methods = {RequestMethod.GET, RequestMethod.POST, RequestMethod.PATCH, RequestMethod.DELETE, RequestMethod.PUT, RequestMethod.OPTIONS})
 public class PokemonController {
 
     @Autowired
